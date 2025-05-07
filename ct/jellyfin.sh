974402e68de03c15ae2b614adc8665e7ce152669
waz000000 <warren.pritchard@gmail.com>
2025-05-07 20:10:27 +0100

    new

--- ct/jellyfin.sh
+++ ct/jellyfin.sh
@@ -41,10 +41,10 @@ msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
 echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8096${CL}"
-PCT enter 100
+echo -e "PCT enter 100
 sudo bash -c 'echo "deb http://deb.debian.org/debian bookworm main non-free non-free-firmware contrib" >> /etc/apt/sources.list'
 apt install build-essential gcc dirmngr ca-certificates software-properties-common apt-transport-https dkms curl -y
 curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/3bf863cc.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/nvidia-drivers.gpg >/dev/null 2>&1
 echo 'deb [signed-by=/usr/share/keyrings/nvidia-drivers.gpg] https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/ /' | sudo tee /etc/apt/sources.list.d/nvidia-drivers.list
 apt update
-reboot
+reboot"
