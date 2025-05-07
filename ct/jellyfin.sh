cd7183cf4ca2eea50cf295e991b33d929e19176f
waz000000 <105541265+waz000000@users.noreply.github.com>
2025-05-07 23:00:26 +0100

    Update jellyfin.sh

--- ct/jellyfin.sh
+++ ct/jellyfin.sh
@@ -29,17 +29,17 @@ function update_script() {
   $STD apt-get update
   $STD apt-get -y upgrade
   # Download the repository signing key and install it to the keyring directory
- $STD curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/3bf863cc.pub | gpg --dearmor --yes --output /usr/share/keyrings/nvidia-drivers.gpg
- $STD echo "deb [signed-by=/usr/share/keyrings/nvidia-drivers.gpg] https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/ /" >> /etc/apt/sources.list.d/nvidia-drivers.list
- $STD echo "deb http://deb.debian.org/debian bookworm main non-free non-free-firmware contrib" >> /etc/apt/sources.list
+ curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/3bf863cc.pub | gpg --dearmor --yes --output /usr/share/keyrings/nvidia-drivers.gpg
+ echo "deb [signed-by=/usr/share/keyrings/nvidia-drivers.gpg] https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/ /" >> /etc/apt/sources.list.d/nvidia-drivers.list
+ echo "deb http://deb.debian.org/debian bookworm main non-free non-free-firmware contrib" >> /etc/apt/sources.list
 # Install Jellyfin and nvidia using the metapackage (which will fetch jellyfin-server, jellyfin-web, and jellyfin-ffmpeg5)
-$STD apt-get update
-$STD apt-get install -y build-essential gcc dirmngr ca-certificates software-properties-common apt-transport-https dkms curl
-$STD apt-get install -y nvidia-driver
-$STD apt-get install -y cuda-driver
-$STD apt-get install -y cuda
-$STD apt-get install -y jellyfin
-$STD apt-get -y --with-new-pkgs upgrade jellyfin jellyfin-server
+apt-get update
+apt-get install -y build-essential gcc dirmngr ca-certificates software-properties-common apt-transport-https dkms curl
+apt-get install -y nvidia-driver
+apt-get install -y cuda-driver
+apt-get install -y cuda
+apt-get install -y jellyfin
+apt-get -y --with-new-pkgs upgrade jellyfin jellyfin-server
 msg_ok "Updated ${APP} LXC"
 exit
 }
@@ -52,10 +52,4 @@ msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
 echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8096${CL}"
-echo -e "PCT enter 100
-sudo bash -c 'echo "deb http://deb.debian.org/debian bookworm main non-free non-free-firmware contrib" >> /etc/apt/sources.list'
-apt install build-essential gcc dirmngr ca-certificates software-properties-common apt-transport-https dkms curl -y
-curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/3bf863cc.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/nvidia-drivers.gpg >/dev/null 2>&1
-echo 'deb [signed-by=/usr/share/keyrings/nvidia-drivers.gpg] https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/ /' | sudo tee /etc/apt/sources.list.d/nvidia-drivers.list
-apt update
-reboot"
+
