53e03b4b8863637c8ad157f6ea975c209b2ab8e5
waz000000 <warren.pritchard@gmail.com>
2025-05-07 19:58:15 +0100

    new

--- ct/jellyfin.sh
+++ ct/jellyfin.sh
@@ -30,6 +30,11 @@ function update_script() {
   $STD apt-get -y upgrade
   $STD apt-get -y --with-new-pkgs upgrade jellyfin jellyfin-server
   msg_ok "Updated ${APP} LXC"
+  sudo bash -c 'echo "deb http://deb.debian.org/debian bookworm main non-free non-free-firmware contrib" >> /etc/apt/sources.list'
+  apt install build-essential gcc dirmngr ca-certificates software-properties-common apt-transport-https dkms curl -y
+  curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/3bf863cc.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/nvidia-drivers.gpg >/dev/null 2>&1
+  echo 'deb [signed-by=/usr/share/keyrings/nvidia-drivers.gpg] https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/ /' | sudo tee /etc/apt/sources.list.d/nvidia-drivers.list
+  apt update
   exit
 }
 
